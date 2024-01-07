92ac0f99e0b068bc851b5c669a87ac62db3d9ffc
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-01-07 23:02:15 +0530

    second commit

---
+++ PassFail.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PassFail {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+    
+            
+        if(marks>=33){
+            System.out.println("pass");
+
+        }
+        else{
+            System.out.println("fail");
+        }
+        
+    }
+}
